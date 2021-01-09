23c53891258398bca1f8b7545315f32ca725dcba
Kiran <kiran.jolly@gmail.com>
2021-01-09 14:17:52 +0000

    increased range of sensor

--- src/boot/ksdk1.1.0/cw5.c
+++ src/boot/ksdk1.1.0/cw5.c
@@ -68,7 +68,7 @@ uint32_t readAccel() {
 	WarpStatus	i2cReadStatus;
 
 	uint16_t        i2cPullUpValue = 32768;
-	int		numberOfReadings = 50;
+	int		numberOfReadings = 44;
 	int		delay = 30; //milliseconds
 
 	uint32_t	startTime;
@@ -79,7 +79,7 @@ uint32_t readAccel() {
 
 	configureSensorMMA8451Q(0x00, 0x01, i2cPullUpValue);
 	writeSensorRegisterMMA8451Q(0x2A, 0x00, i2cPullUpValue); //set standby mode
-	writeSensorRegisterMMA8451Q(0x0E, 0x01, i2cPullUpValue); //set 4g scale - can only do this in standby modeoe
+	writeSensorRegisterMMA8451Q(0x0E, 0x02, i2cPullUpValue); //set 4g scale - can only do this in standby modeoe
 	writeSensorRegisterMMA8451Q(0x2A, 0x01, i2cPullUpValue); //set actve mode
 
 	for (int i=0; i<numberOfReadings; i++) {
@@ -94,7 +94,7 @@ uint32_t readAccel() {
 			readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 6) | (readSensorRegisterValueLSB >> 2);
 			readSensorRegisterValueCombined = (readSensorRegisterValueCombined ^ (1 << 13)) - (1 << 13);
 			
-			xdata[i] = (int) (readSensorRegisterValueCombined/208 + 0.5);
+			xdata[i] = (int) (readSensorRegisterValueCombined/100 + 0.5);
 		}	
 		else {
 //			SEGGER_RTT_printf(0, "\nError reading register");
@@ -111,29 +111,37 @@ uint32_t readAccel() {
 	int16_t 	velocity[numberOfReadings-1];
 	int16_t 	distance[numberOfReadings-2];
 
+	float		intermediateForCalcs;
+
 	for (int i=0; i<numberOfReadings; i++) {
+
+		SEGGER_RTT_printf(0, "\na: %d", xdata[i]);
 
 		//get maximum acceleration
 		if (abs(xdata[i]) > maxAccel) {
 			maxAccel = abs(xdata[i]);
-			maxAccelIndex = i;	
+			maxAccelIndex = i;
 		}
 
 		if (i < numberOfReadings-1) {
-			velocity[i] = (xdata[i] + xdata[i+1]) * delay/20;
+			intermediateForCalcs = (float) (xdata[i] + xdata[i+1]) * (float) delay/20;
+			velocity[i] = (int) intermediateForCalcs;
 			if (i > 0) {
 				velocity[i] += velocity[i-1];
 
-				distance[i-1] = (velocity[i-1] + velocity[i]) * delay/2000;
+				intermediateForCalcs = (float) ((velocity[i-1] + velocity[i]) * delay) /2000;
+
+				distance[i-1] = (int) intermediateForCalcs;
 				if (i > 1) {
 					distance[i-1] += distance[i-2];
-				}		
+				}
+				SEGGER_RTT_printf(0, "\nd: %d", distance[i-1]);		
 			}
+			SEGGER_RTT_printf(0, "\nv: %d", velocity[i]);
 		}
 	}
 
         
-	SEGGER_RTT_printf(0, "Acceleration on impact is %dm/s^2\n", maxAccel);
 	if (maxAccel > 99) {maxAccel = 99;}
 	uint32_t returnVal = maxAccel;
 
@@ -143,7 +151,7 @@ uint32_t readAccel() {
 	}
 
 
-	SEGGER_RTT_printf(0, "Velocity at impact is %dcm/s\n\n", velocity[maxAccelIndex]);
+//	SEGGER_RTT_printf(0, "Velocity at impact is %dcm/s\n\n", velocity[maxAccelIndex]);
 	if (abs(velocity[maxAccelIndex]) > 99) {velocity[maxAccelIndex] = 99;}
 	returnVal += abs(velocity[maxAccelIndex]) * 100;
 
@@ -153,15 +161,15 @@ uint32_t readAccel() {
 	}
 
 
-	SEGGER_RTT_printf(0, "Distance before impact is %dcm", distance[maxAccelIndex]);
+//	SEGGER_RTT_printf(0, "Distance before impact is %dcm", distance[maxAccelIndex]);
 	if (abs(distance[maxAccelIndex]) > 99) {distance[maxAccelIndex] = 99;}
 	returnVal += abs(distance[maxAccelIndex]) * 10000;
 
 
-	SEGGER_RTT_printf(0, "Distance after impact is %dcm\n", distance[numberOfReadings-3]-distance[maxAccelIndex]);
+//	SEGGER_RTT_printf(0, "Distance after impact is %dcm\n", distance[numberOfReadings-3]-distance[maxAccelIndex]);
 	if (abs(distance[numberOfReadings-3]-distance[maxAccelIndex]) > 99) {distance[numberOfReadings-3] = 99+distance[maxAccelIndex];}
 	returnVal += abs(distance[numberOfReadings-3]-distance[maxAccelIndex]) * 1000000 + 100000000;
-	
+
 	return returnVal;
 }
 
